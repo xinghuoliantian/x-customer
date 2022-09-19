bf85678c0f4cec117239ffe1a60a69916b537d99
Administrator <f25245721@qq.com>
2022-09-19 10:41:54 +0800

    initialization

--- src/main/java/com/gcfw/equipment/controller/EquController.java
+++ src/main/java/com/gcfw/equipment/controller/EquController.java
@@ -8,9 +8,7 @@ import com.gcfw.equipment.service.EquService;
 import com.gcfw.equipment.vo.EquQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @Controller
